a504f4647f1076ed8f4279849dcb559f6f4afdb1
loopy750 <35911135+loopy750@users.noreply.github.com>
2020-07-28 17:16:19 +1000

    Add files via upload

--- modifications/rtmp_stream_2.cmd
+++ modifications/rtmp_stream_2.cmd
@@ -1,1 +1,8 @@
-@START "mpv - stream2" /MIN mpv --vd-lavc-threads=1 --cache-pause=no --demuxer-lavf-analyzeduration=1 --no-osd-bar --no-osc --input-media-keys=no --window-minimized=yes --fs --force-window --audio-device=auto -vf lavfi=[fade=in:10:50] rtmp://127.0.0.1/live/stream2
+:Start
+@ECHO OFF
+ECHO Waiting for connection...
+ECHO.
+mpv --profile=rtmpstream rtmp://127.0.0.1/live/stream2
+TIMEOUT /T 10
+ECHO.
+GOTO Start &:: Restart in an extremely rare case where mpv is shut down because of EOF
