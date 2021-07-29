3940c10a2dd42c99fc30ef0038da206ba381fb7d
loopy750 <35911135+loopy750@users.noreply.github.com>
2021-07-29 23:35:26 +1000

    Typo

--- modifications/mpv/rtmp_stream_1.cmd
+++ modifications/mpv/rtmp_stream_1.cmd
@@ -2,7 +2,7 @@
 @ECHO OFF
 ECHO Waiting for connection...
 ECHO.
-%~dp0mpv --profile=rtmpstream rtmp://127.0.0.1/live/stream1
+"%~dp0mpv" --profile=rtmpstream rtmp://127.0.0.1/live/stream1
 TIMEOUT /T 10
 ECHO.
 GOTO Start &:: Restart in an extremely rare case where mpv is shut down because of EOF
